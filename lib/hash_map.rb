e4c604a858dd4d667c8839f78a30038616ed92ca
Blake <blameswalker@gmail.com>
2024-01-29 23:11:48 -0800

    Define #hash method that takes a string and converts it into an integer

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -7,4 +7,10 @@ class HashMap
     @buckets = Array.new(size) { [] }
     @load_factor = load_factor
   end
+
+  def hash(string)
+    hash_code = 0
+    string.each_char {|char| hash_code = hash_code * 31 + char.ord}
+    hash_code % buckets.size
+  end
 end
--- main.rb
+++ main.rb
@@ -4,3 +4,6 @@ require_relative 'lib/hash_map'
 
 hash_map = HashMap.new
 p hash_map.buckets
+
+p hash_map.hash('blake walker')
+p hash_map.hash('blaek wakler')
